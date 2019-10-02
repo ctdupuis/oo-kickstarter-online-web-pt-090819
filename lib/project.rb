833241d845514771a5fb952887c948e569d63822
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-02 20:25:32 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,8 +2,8 @@ class Project
   attr_accessor :name 
   attr_reader :backers
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
   end
 end
